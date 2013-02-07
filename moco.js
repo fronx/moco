7363ee0881da7ada7e56747c9f0eb3666907895b
Fronx <fronx@soundcloud.com>
2013-02-07 13:48:33 +0100

    parse

--- moco.js
+++ moco.js
@@ -1,9 +1,25 @@
 (function(d){
+  function syncGet (url) {
+    console.log(url);
+    var request = new XMLHttpRequest();
+    request.open('GET', url, false);
+    request.send(null);
+    if (request.status === 200) {
+      return request.responseText;
+    }
+  }
+  // gather info
   var rawUrl = d.getElementById('raw-url').href;
+  console.log(rawUrl);
+
+  // remove all the content!!!
   d.getElementById('wrapper').remove();
   d.getElementById('footer').remove();
+
+  // the post-content world
   d.body
     .appendChild(d.createElement('script'))
     .src = 'https://raw.github.com/marijnh/acorn/master/acorn.js';
-  console.log(rawUrl);
+  var code = syncGet(rawUrl);
+  console.log(acorn.parse(code));
 })(document);
--- notes.md
+++ notes.md
@@ -33,3 +33,6 @@ sooo... use a parser?
   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
   <span class="vi">@env</span>      <span class="o">=</span> <span class="no">EnvSet</span><span class="o">.</span><span class="n">new</span><span class="p">(</span><span class="n">env_set_repo</span><span class="o">.</span><span class="n">expand</span><span class="p">(</span><span class="n">data</span><span class="o">[</span><span class="s1">'env'</span><span class="o">]</span> <span class="o">||</span> <span class="p">{}))</span></div>
 ```
+
+## todo
+- read file content, store it in a variable, pass it to the parser
