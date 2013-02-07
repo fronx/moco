8aaf0e302aaa5adafcc4e03624b411adb24d6d7a
Fronx <fronx@soundcloud.com>
2013-02-07 14:44:37 +0100

    use github api

--- moco.js
+++ moco.js
@@ -1,4 +1,22 @@
 (function(d){
+  // http://stackoverflow.com/questions/2820249/base64-encoding-and-decoding-in-client-side-javascript
+  function decodeBase64 (s) {
+    var e={},i,k,v=[],r='',w=String.fromCharCode;
+    var n=[[65,91],[97,123],[48,58],[43,44],[47,48]];
+
+    for(z in n){for(i=n[z][0];i<n[z][1];i++){v.push(w(i));}}
+    for(i=0;i<64;i++){e[v[i]]=i;}
+
+    for(i=0;i<s.length;i+=72){
+      var b=0,c,x,l=0,o=s.substring(i,i+72);
+      for(x=0;x<o.length;x++){
+        c=e[o.charAt(x)];b=(b<<6)+c;l+=6;
+          while(l>=8){r+=w((b>>>(l-=8))%256);}
+      }
+    }
+    return r;
+  }
+
   function get (url, fn) {
     console.log(url);
     var request = new XMLHttpRequest();
@@ -10,7 +28,7 @@
     request.send(null);
   }
   // gather info
-  var rawUrl = d.getElementById('raw-url').href;
+  var rawUrl = "https://api.github.com/repos/marijnh/acorn/contents/acorn.js"; // d.getElementById('raw-url').href;
 
   // remove all the content!!!
   d.getElementById('wrapper').remove();
@@ -21,6 +39,9 @@
     .appendChild(d.createElement('script'))
     .src = 'https://raw.github.com/marijnh/acorn/master/acorn.js';
   get(rawUrl, function (code) {
+    doc  = JSON.parse(code);
+    code = decodeBase64(doc['content'].split("\n").join(''));
+    console.log(code);
     console.log(acorn.parse(code));
   });
 })(document);
